5deebfdca6ad776692fefb85fe8c50a3fda0d5c6
gschnurr <gibschnurr@gmail.com>
2019-09-12 16:31:29 +0200

    looping function is work still working on add the ability to save the data and move to a new sheet, currently not working. that bit of code has been commented out.

--- pp_bo_form.js
+++ pp_bo_form.js
@@ -11,10 +11,17 @@ function pp_form_gen() {
   var spreadsheet = SpreadsheetApp.getActive();
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var ui = SpreadsheetApp.getUi();
+  var ppe = ss.getSheetByName('PayPal Extract');
+  var ppeLr = ppe.getLastRow();
+  var ppeLc = ppe.getLastColumn();
+  var ppeTitleColumnArr = ppe.getRange(1, 1, 1, ppeLc).getValues();
+
+  //this array contains the choices for vendor category
   var vendorCatArr = ['Agencies', 'Commercial Partners', 'Credit Reference and Fraud Agencies',
   'Customer Service Outsourcing', 'Financial Products', 'General', 'Legal', 'Marketing and PR',
   'Operational Services', 'Payment Processors'];
 
+  //this array contains the choices for purpose
   var purposeCatArr = ['To provide our services and products, to fulfill relevant agreements with our merchants and to otherwise administer our business relationship with our merchants.',
   'To confirm your identity and verify our merchant’s personal and contact details.',
   'To prove that transactions have been executed.', 'To establish, exercise or defend a legal claim or collection procedures.',
@@ -33,35 +40,67 @@ function pp_form_gen() {
   'Risk management obligations such as credit performance and quality, insurance risks and compliance with capital adequacy requirements under applicable law.',
   'To administer payments carried out by using our services from a Merchant.', 'To communicate with our merchants in relation to our services.'];
 
-  var ppe = ss.getSheetByName('PayPal Extract');
-  var ppeLr = ppe.getLastRow();
-  var ppeLc = ppe.getLastColumn();
-  var ppeTitleColumnArr = ppe.getRange(1, 1, 1, ppeLc).getValues();
+  //this loops checks all of the current sheets in the spreadsheet for the spreadsheet savePointSheet
+  var savePointSheet = 'PayPal Extract Save';
+  Logger.log('All static variables have been initialized.')
 
+  for (var s = 0; s < sheets.length; s++){
+    curSheetName = sheets[s].getSheetName();
+    if (curSheetName == savePointSheet) {
+      Logger.log('Sheet with sheet name ' + savePointSheet + ' already exists.')
+      break;
+    }
+    else if (curSheetName != savePointSheet && s != (sheets.length - 1)) {
+      continue;
+    }
+    else if (curSheetName != savePointSheet && s == (sheets.length - 1)){
+      spreadsheet.insertSheet(1);
+      spreadsheet.getActiveSheet().setName('PayPal Extract Save');
+      var ppeSave = ss.getSheetByName('PayPal Extract Save');
+      ppeSave.getRange('A1').activate();
+      ppe.getRange(1, 1, 1, ppeLc).copyTo(ppeSave.getActiveRange(), SpreadsheetApp.CopyPasteType.PASTE_VALUES, false);
+      ppeSave.getRange('A1').activate();
+      ppe.getRange(1, 1, 1, ppeLc).copyTo(ppeSave.getActiveRange(), SpreadsheetApp.CopyPasteType.PASTE_FORMAT, false);
+      ppeSave.setFrozenRows(1);
+      Logger.log('Sheet with sheet name ' + savePointSheet + ' created and formatted.')
+    }
+    else {
+      ui.alert('OOPs: something went wrong. Please contact and administrator.');
+    }
+  }
+
+  var ppeSaveLr = ppeSave.getLastRow();
+  var ppeSaveLc = ppeSave.getLastColumn();
+  var ppeSaveLrPOne = ppeSaveLr + 1;
+  Logger.log('ppeSave variables initialized.')
+
+  //creates 1d array of the title row values for the ppe spreadsheet
   var ppeOned = flatten_arr(ppeTitleColumnArr);
-
+  Logger.log('PPE title row array created.')
+  //finds the position of the business system owner column grabs all of the data in that column and creates a 1d array and removes the last blank value
   var busSysOwnColPos = find_col(ppeOned, 'Business System Owner');
   var busSysOwnerArr = ppe.getRange(2, busSysOwnColPos, ppeLr, 1).getValues();
   var bsoOned = flatten_arr(busSysOwnerArr);
   bsoOned.pop();
-
+  Logger.log('One dimensional BO array created.')
+  //finds the application column and creates a 1d array of all the applications currently not used in this function
   var appNameColPos = find_col(ppeOned, 'Application');
   var appNameArr = ppe.getRange(2, appNameColPos, ppeLr, 1).getValues();
   var anOned = flatten_arr(appNameArr);
 
-  //this is an array of the row number of the applications owned by the currentBoInLoop
-  //creates an arr of all col pos of columns to be updated
+  //creates an arr of all col pos of columns to be placed in the form and updated
   var gdprBoColPosArr = [];
   for (var c = 0; c < gdprBoTColArr.length; c++) {
     var gdprBoColPos = find_col(ppeOned, gdprBoTColArr[c]);
     gdprBoColPosArr.push(gdprBoColPos);
   }
+  Logger.log('GDPR Column positions to be updated from initial static array have been found.')
   //an array of the bo's who have been looped through to prevent creating multiple forms for one user
-  var boFormComArr = ['placeholder', ''];
+  var boFormComArr = [''];
   //loop through the business owners arr
   for (var b = 0; b < bsoOned.length; b++) {
     Logger.log('BOArry Loop, BO = ' + bsoOned[b] + ' b = ' + b);
-    Logger.log(boFormComArr.length + ' number of forms have been sent.')
+    Logger.log((boFormComArr.length - 1) + ' forms have been sent.')
     //check the current iteration of the bo arr against the bo complete? arr
     for (var c = 0; c < boFormComArr.length; c++) {
       Logger.log('BOs that have been used are ' + boFormComArr);
@@ -191,7 +230,26 @@ function pp_form_gen() {
           }
           Logger.log('All items are added for ' + appToUpdate);
         }
-        Logger.log('All applications are added to the form. ' + u + ' applications were added.'');
+        /*
+        this part is to delete the used rows and is currently underconstruction
+        for (var f = 0; f < bsoOned.length; f++) {
+          if (bsoOned[f] == currentBoInLoop) {
+            var ppeSaveLr = ppeSave.getLastRow();
+            var ppeSaveLc = ppeSave.getLastColumn();
+            var ppeSaveLrPOne = ppeSaveLr + 1;
+            var cwuAppRow = f + 2;
+            var ppeRowToCopy = ppe.getRange(cwuAppRow, 1, 1, ppeLc).getValues();
+            ppeSave.getRange(ppeSaveLrPOne, 1, 1, ppeSaveLc).setValues(ppeRowToCopy);
+            ppe.deleteRow(cwuAppRow);
+            var appNameCell = ppe.getRange(cwuAppRow, appNameColPos, 1, 1).getValue();
+            Logger.log(appNameCell + ' has been deleted from ppe and copied to ppeSave.');
+          }
+          else {
+            continue;
+          }
+        }
+        **/
+        Logger.log('All applications are added to the form. ' + u + ' applications were added.');
         ScriptApp.newTrigger('on_Form_Sub_Bo_Trigger')
           .forForm(userUpdatesForm)
           .onFormSubmit()
