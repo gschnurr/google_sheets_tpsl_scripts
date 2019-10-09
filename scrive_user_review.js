b59c92167d6d87e1cbd3b6c30536df0d8535bb13
gschnurr <gibschnurr@gmail.com>
2019-10-09 16:39:16 +0200

    started working on scrive project

--- scrive_user_review.js
+++ scrive_user_review.js
@@ -6,3 +6,27 @@ create a script that will grab all of the answers to the forms and based on form
 Script to check what surveys have been responded to
 script to send reminder
 **/
+
+
+function get_usage() {
+  var csu = ss.getSheetByName('Current Scrive Users');
+  var csuLr = csu.getLastRow();
+  var csuLc = csu.getLastColumn();
+  var cau = ss.getSheetByName('Current Active Users');
+  var cauLr = cau.getLastRow();
+  var cauLc = cau.getLastColumn();
+  var fqs = ss.getSheetByName('Form Questions');
+
+  var csuTcaArr = csu.getRange(1, 1, 1, csuLc).getValues();
+  var csuTcaOned = flatten_arr(csuTcaArr);
+  var csuUserColPos = find_col(csuTcaOned, 'User');
+  var csuUserArr = csu.getRange(2, csuUserColPos, csuLr, 1).getValues();
+  var csuUserOned = flatten_arr(csuUserArr);
+
+  var cauTcaArr = cau.getRange(1, 1, 1, cauLc).getValues();
+  var cauTcaOned = flatten_arr(cauTcaArr);
+  var cauUserColPos = find_col(cauTcaOned, 'User');
+  var cauUserArr = cau.getRange(2, cauUserColPos, cauLr, 1).getValues();
+  var cauUserOned = flatten_arr(cauUserArr);
+
+}
