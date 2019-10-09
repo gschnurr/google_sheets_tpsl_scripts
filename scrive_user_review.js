01041b4ed5a4a25871a37fe5ff07278c9b20d234
gschnurr <gibschnurr@gmail.com>
2019-10-09 14:36:45 +0200

    began a new project

---
+++ scrive_user_review.js
@@ -0,0 +1,8 @@
+/*
+Create a script that pulls in the usage and last usage month by user
+set a threshold for when we might want to turn off someones use of Scrive (not using in the last 3 months requires a reach out)
+create a form that is sent out to each user that asks why they use scrive and for what team.
+create a script that will grab all of the answers to the forms and based on form title and format the answers in spreadsheet
+Script to check what surveys have been responded to
+script to send reminder
+**/
