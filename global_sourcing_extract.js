d99cb1e5f38e15e647a0222c0cac25b75a6285d3
Gibson Schnurr <gibschnurr@gmail.com>
2020-03-26 11:43:12 +0100

    I have almost finished gs extract and validation macro just need a few finishing touches and test

--- global_sourcing_extract.js
+++ global_sourcing_extract.js
@@ -1,8 +1,3 @@
-//3 Copy this into a specific workbook for this purpose as a new sheet. From there run the check against the older version.
-
-//4 check function should be in all extracts but will copy over the old vs new in the same workbook
-//  use the old tpsl macro way to check for changes + we will also need to check if there are any new SL-IDS entirely.
-
 function gs_extract() {
   var spreadsheet = SpreadsheetApp.getActive();
   var ss = SpreadsheetApp.getActiveSpreadsheet();
@@ -11,9 +6,9 @@ function gs_extract() {
   'Last Notice Period', 'Agreement End Date', 'Agreement Transition Status', 'Yearly Agreement Value (SEK)', 'Comments'];
   //create extract sheet and name
   spreadsheet.insertSheet(2);
-  spreadsheet.getActiveSheet().setName('Global Services Extract');
+  spreadsheet.getActiveSheet().setName('Global Services New Extract');
 
-  var gsExtract = ss.getSheetByName('Global Services Extract');
+  var gsExtract = ss.getSheetByName('Global Services New Extract');
   //paste value of specified range
   var tpslAllCellsData = tpslAllCells.getValues();
   var tpslAllNotes = tpslAllCells.getNotes();
@@ -76,4 +71,121 @@ function gs_extract() {
 
   filter_rows(gsExtractContractDataExistsArr, gsExtRowFilArr, gsExtract);
 
+  gsExtract.copyTo('1bz7mzCqrBJNfNsAl7FPsUJ6dgov703mTanfqIoywC-4');
+
 };
+
+
+
+function gs_extract_data_verification() {
+  var spreadsheet = SpreadsheetApp.getActive();
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var ui = SpreadsheetApp.getUi();
+
+  var gseNew = ss.getSheetByName('Global Services New Extract');
+  var gseNewLr = gseNew.getLastRow();
+  var gseNewLc = gseNew.getLastColumn();
+  var gseNewTca = gseNew.getRange(1, 1, 1, gseNewLc).getValues();
+  var gseNewTconed = flatten_arr(gseNewTca);
+  var gseNewIdColPos = find_col(gseNewTconed, 'SL-ID');
+  var gseNewIdArr = gseNew.getRange(2, gseNewIdColPos, gseNewLr, 1).getValues();
+  var gseNewIdOned = flatten_arr(gseNewIdArr);
+
+  var gseOld = ss.getSheetByName('Global Services Last Extract');
+  var gseOldLr = gseOld.getLastRow();
+  var gseOldLc = gseOldLc.getLastColumn();
+  var gseOldTca = gseOld.getRange(1, 1, 1, gseOldLc).getValues();
+  var gseOldTconed = flatten_arr(gseOldTca);
+  var gseOldIdColPos = find_col(gseOldTconed, 'SL-ID');
+  var gseOldIdArr = gseOld.getRange(2, gseOldIdColPos, gseOldLr, 1).getValues();
+  var gseOldIdOned = flatten_arr(gseOldIdArr);
+
+//beginning of validating the new data against the old data by looping through the new data IDs
+  for (var ln = 0; ln < gseNewIdOned.length; ln++) {
+    var gseNewId = gseNewIdOned[ln];
+    var gseNewIdRow = ln + 2;
+
+    if (gseOldIdOned.indexOf(gseNewId) > -1) {
+      var gseOldIdIndex = gseOldIdOned.indexOf(gseNewId);
+      var gseOldIdRow = gseOldIdIndex + 2;
+
+      for (var md = 0; md < gseNewTconed.length; md++) {
+        var colTtm = gseNewTconed[md];
+        var gseNewColPos = md + 1;
+
+        if (gseOldTconed.indexOf(colTtm) > -1) {
+          var gseColTitleIndex = gseOldTconed.indexOf(colTtm);
+          var gseOldColPos = gseColTitleIndex + 1;
+          var gseOldVal = gseOld.getRange(gseOldIdRow, gseOldColPos, 1, 1).getValue();
+          var gseNewCellPos = gseNew.getRange(gseNewIdRow, gseNewColPos, 1, 1);
+          var gseNewVal = gseNewCellPos.getValue();
+
+          if (gseNewVal == gseOldVal) {
+            continue;
+          }
+          else {
+            gseNewCellPos.setBackground('yellow');
+          } //comparing values if statement
+        } //finding matching column title if statement
+        else {
+          continue;
+        }
+      } //using already found approw data to find exact cells to validate for loop (TCA Loop)
+    } //if statement for seeing if there is a matching ID or not in the old sheet from the new sheet
+    else {
+      gseNew.getRange(gseNewIdRow, 1, 1, gseNewLc).setBackground('green');
+    } //else statement if the new extract ID does not exist in the old data this means its a new system
+  } // for loop of new sheet IDs
+
+//this for loop is checking if a system has been deleted from the tpsl since the last extract by checking if all the
+//old ids exist in the new sheet if they dont it will add the old data to the bottom of the sheet and highlight red
+  for (var lo = 0; lo < gseOldIdOned.length; lo++) {
+    var gseMissingOldRow = lo + 2;
+    if (gseNewIdOned.indexOf(gseOldIdOned[lo]) > -1) {
+      continue;
+    }
+    else {
+      var gseNewAddLr = gseNew.getLastRow();
+      var gseNewEmptyRow = gseNewAddLr + 1;
+
+      for (var ao = 0; ao < gseOldTconed.length; ao++) {
+        var colTtm = gseOldTconed[ao];
+        var gseMissingOldColPos = ao + 1;
+
+        if (gseNewTconed.indexOf(colTtm) > -1) {
+          var gseNewEmptyRowColPos = gseNewTconed.indexOf(colTtm) + 1;
+          var gseMissingOldVal = gseOld.getRange(gseMissingOldRow, gseMissingOldColPos, 1, 1).getValue();
+          gseNew.getRange(gseNewEmptyRow, gseNewEmptyRowColPos, 1, 1).setValue(gseMissingOldVal);
+          gseNew.getRange(gseNewEmptyRow, gseNewEmptyRowColPos, 1, 1).setBackground('red');
+        }
+        else {
+          continue;
+        } // if index match closing on col pos
+      } //for loop for col pos and replace closed
+    } //else if there is no matching id value in new sheet
+  } // closing of for loop looking for deleted applications
+// need to change the name of the new sheet that is to be delivered then create a copy in its own workbook
+//create an email and add that new copy as attachment to the email with the email body explaining colors
+// need to change the name of the old extract we compared against to be archived something then change the latest one to the old name
+
+}//function end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//bullshit comment delete lolz=
